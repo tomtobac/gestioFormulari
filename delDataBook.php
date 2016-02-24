0f23cd6433490bafa819c423f577cace1293c6fa
tom <somethingofhappy@gmail.com>
2016-02-24 14:57:54 +0100

    haton sucks

---
+++ delDataBook.php
@@ -0,0 +1,39 @@
+<?php
+require_once "funcions.php";
+$mysqli = conecta();
+
+/*
+ * GET ALL DATA FROM REQUEST {OP WAY}
+ */
+foreach ($_REQUEST as $key => $value) {
+    ${$key} = !empty($_REQUEST[$key]) ? $value : NULL;
+    //echo $key . " : " . ${$key} . "<br>";
+}
+
+if (isset($id_llib) && isset($id_autor)){
+
+	$delete = "DELETE FROM LLI_AUT WHERE FK_IDLLIB = ? AND FK_IDAUT = ?";
+	$stmt = $mysqli->prepare($delete);
+	$stmt->bind_param("ii", $id_llib, $id_autor) or die($mysqli->error . __LINE__);
+	$prompt = " relació entre l'autor i el llibre!";
+
+}else if(isset($id_llib) && isset($num_exm)){
+
+	$delete = "DELETE FROM EXEMPLARS WHERE FK_IDLLIB = ? AND NUM_EXM = ?";
+	$stmt = $mysqli->prepare($delete);
+	$stmt->bind_param("ii", $id_llib, $num_exm) or die($mysqli->error . __LINE__);
+	$prompt = " l'exemplar d'aquest llibre!";
+}
+
+
+if ($stmt->execute()) { 
+   // it worked
+	$result = "<span class='glyphicon glyphicon-ok' aria-hidden='true'></span> <strong>Esborrat</strong>" . $prompt;
+} else {
+   // it didn't
+	$result = $stmt->error;
+}
+
+echo $result;
+
+?>
--- js/main.js
+++ js/main.js
@@ -11,34 +11,31 @@ function init() {
 
 function getBooks() {
     $.ajax({
-            url: 'getBooks.php',
-            type: 'GET',
-            dataType: 'json',
-        })
-        .done(function(data) {
-            //console.log("success");
-            $.each(data, function(key, val) {
-                var _table = $('#llistatLibres');
-                var row;
-                row += '<tr>';
-                row += '<td>' + val.ID_LLIB + '</td>';
-                row += '<td>' + val.TITOL + '</td>';
-                row += '<td>';
-                row += '<button type="button" class="btn btn-info btn-block" data-toggle="modal" data-target="#myModal" value=' + val.ID_LLIB + '><span class="glyphicon glyphicon-search"></span> Mostra</button>';
-                row += '</td>';
-                row += '</tr>'
-                _table.append(row);
-            });
-            //Activam es botons
-            $('.btn-info').click(showBook);
-        })
-        .fail(function() {
-            //console.log("error");
-            $('.alert alert-danger').show();
-        })
-        .always(function() {
-            //console.log("complete");
+        url: 'getBooks.php',
+        type: 'GET',
+        dataType: 'json',
+    }).done(function(data) {
+        //console.log("success");
+        $.each(data, function(key, val) {
+            var _table = $('#llistatLibres');
+            var row;
+            row += '<tr>';
+            row += '<td>' + val.ID_LLIB + '</td>';
+            row += '<td>' + val.TITOL + '</td>';
+            row += '<td>';
+            row += '<button type="button" class="btn btn-info btn-block" data-toggle="modal" data-target="#myModal" value=' + val.ID_LLIB + '><span class="glyphicon glyphicon-search"></span> Mostra</button>';
+            row += '</td>';
+            row += '</tr>'
+            _table.append(row);
         });
+        //Activam es botons
+        $('.btn-info').click(showBook);
+    }).fail(function() {
+        //console.log("error");
+        $('.alert alert-danger').show();
+    }).always(function() {
+        //console.log("complete");
+    });
 }
 
 function showBook(e) {
@@ -50,121 +47,122 @@ function showBook(e) {
 function getDataBook(ID_LLIBRE) {
     cleanFormData();
     $.ajax({
-            url: 'getDataBook.php',
-            type: 'GET',
-            dataType: 'json',
-            data: { ID_LLIB: ID_LLIBRE },
-        })
-        .done(function(data) {
-            //console.log("success");
-            //console.log($(data.LLIBRE));
-            $.each(data.LLIBRE, function(index, el) {
-                $('#CodiLlibre').val(el.ID_LLIB);
-                $('#Titol').val(el.TITOL || '');
-                $('#NumEdicio').val(el.NUMEDICIO || '');
-                $('#AnyEdicio').val(el.ANYEDICIO || '');
-                $('#Descripcio').val(el.DESCRIP_LLIB || '');
-                $('#ISBN').val(el.ISBN || '');
-                $('#DepositLegal').val(el.DEPLEGAL || '');
-                $('#SigTop').val(el.SIGNTOP || '');
-            });
-            $.each(data.COLLECCIONS, function(index, el) {
-                //  <option value="1">Option one</option>
-                $('#collecio')
-                    .append($("<option></option>")
-                        .attr("value", el.COLLECCIO)
-                        .text(el.COLLECCIO));
-                if (data.LLIBRE[0].FK_COLLECCIO != null && data.LLIBRE[0].FK_COLLECCIO == el.COLLECCIO) {
-                    $('#collecio option:last').attr("selected", "selected");
-                }
-            });
-            $.each(data.DEPARTAMENTS, function(index, el) {
-                $('#Departament')
-                    .append($("<option></option>")
-                        .attr("value", el.DEPARTAMENT)
-                        .text(el.DEPARTAMENT));
-                if (data.LLIBRE[0].FK_DEPARTAMENT != null && data.LLIBRE[0].FK_DEPARTAMENT == el.DEPARTAMENT) {
-                    $('#Departament option:last').attr("selected", "selected");
-                }
-            });
-            $.each(data.EDITORS, function(index, el) {
-                $('#Editorial')
-                    .append($("<option></option>")
-                        .attr("value", el.ID_EDIT)
-                        .text(el.NOM_EDIT));
-                if (data.LLIBRE[0].FK_IDEDIT != null && data.LLIBRE[0].FK_IDEDIT == el.ID_EDIT) {
-                    $('#Editorial option:last').attr("selected", "selected");
-                }
-            });
-            $.each(data.LLENGUES, function(index, el) {
-                $('#Llengua')
-                    .append($("<option></option>")
-                        .attr("value", el.LLENGUA)
-                        .text(el.LLENGUA));
-                if (data.LLIBRE[0].FK_LLENGUA != null && data.LLIBRE[0].FK_LLENGUA == el.LLENGUA) {
-                    $('#Llengua option:last').attr("selected", "selected");
-                }
-            });
-            $.each(data.AUTORS, function(index, el) {
-                var _table = $('#autorsModal');
-                var row;
-                row += '<tr>';
-                row += '<td>' + el.ID_AUT + '</td>';
-                row += '<td>' + el.NOM_AUT + '</td>';
-                row += '<td>' + el.DNAIX_AUT + '</td>';
-                row += '<td>' + el.FK_NACIONALITAT + '</td>';
-                row += '<td class=\'taulaEditar\'>';
-                row += '<button type="button" class="btn btn-danger btn-block" value=' + el.ID_AUT + '><span class="glyphicon glyphicon-trash"></span> Borrar</button>';
-                row += '</td>';
-                row += '</tr>'
-                _table.append(row);
-            });
-            $.each(data.EXEMPLARS, function(index, el) {
-                var _table = $('#exemplarsModals');
-                var row;
-                row += '<tr>';
-                row += '<td>' + el.NUM_EXM + '</td>';
-                row += '<td>' + el.NREG + '</td>';
-                row += '<td>' + el.DATALTA_EXM + '</td>';
-                row += '<td>' + el.FK_UBICEXM + '</td>';
-                row += '<td class=\'taulaEditar\'>';
-                row += '<button type="button" class="btn btn-danger btn-block" value=' + el.NREG + '><span class="glyphicon glyphicon-trash"></span> Borrar</button>';
-                row += '</td>';
-                row += '</tr>'
-                _table.append(row);
-            });
-            blockStuff(true);
-
-            $('#botoModal123').off("click").text("Editar");
-            $('#botoModal123').on("click", function() {
-                blockStuff(false);
-                $('.taulaEditar').show();
-                $(this).text('Guardar');
-                $(this).off("click").on("click", function() {
-                    saveDataForm();
-                });
-            });
-
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            //console.log("complete");
+        url: 'getDataBook.php',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+            ID_LLIB: ID_LLIBRE
+        },
+    }).done(function(data) {
+        //console.log("success");
+        //console.log($(data.LLIBRE));
+        $.each(data.LLIBRE, function(index, el) {
+            $('#CodiLlibre').val(el.ID_LLIB);
+            $('#Titol').val(el.TITOL || '');
+            $('#NumEdicio').val(el.NUMEDICIO || '');
+            $('#AnyEdicio').val(el.ANYEDICIO || '');
+            $('#Descripcio').val(el.DESCRIP_LLIB || '');
+            $('#ISBN').val(el.ISBN || '');
+            $('#DepositLegal').val(el.DEPLEGAL || '');
+            $('#SigTop').val(el.SIGNTOP || '');
         });
+        $.each(data.COLLECCIONS, function(index, el) {
+            //  <option value="1">Option one</option>
+            $('#collecio').append($("<option></option>").attr("value", el.COLLECCIO).text(el.COLLECCIO));
+            if (data.LLIBRE[0].FK_COLLECCIO != null && data.LLIBRE[0].FK_COLLECCIO == el.COLLECCIO) {
+                $('#collecio option:last').attr("selected", "selected");
+            }
+        });
+        $.each(data.DEPARTAMENTS, function(index, el) {
+            $('#Departament').append($("<option></option>").attr("value", el.DEPARTAMENT).text(el.DEPARTAMENT));
+            if (data.LLIBRE[0].FK_DEPARTAMENT != null && data.LLIBRE[0].FK_DEPARTAMENT == el.DEPARTAMENT) {
+                $('#Departament option:last').attr("selected", "selected");
+            }
+        });
+        $.each(data.EDITORS, function(index, el) {
+            $('#Editorial').append($("<option></option>").attr("value", el.ID_EDIT).text(el.NOM_EDIT));
+            if (data.LLIBRE[0].FK_IDEDIT != null && data.LLIBRE[0].FK_IDEDIT == el.ID_EDIT) {
+                $('#Editorial option:last').attr("selected", "selected");
+            }
+        });
+        $.each(data.LLENGUES, function(index, el) {
+            $('#Llengua').append($("<option></option>").attr("value", el.LLENGUA).text(el.LLENGUA));
+            if (data.LLIBRE[0].FK_LLENGUA != null && data.LLIBRE[0].FK_LLENGUA == el.LLENGUA) {
+                $('#Llengua option:last').attr("selected", "selected");
+            }
+        });
+        $.each(data.AUTORS, function(index, el) {
+            var _table = $('#autorsModal');
+            var row;
+            row += '<tr>';
+            row += '<td>' + el.ID_AUT + '</td>';
+            row += '<td>' + el.NOM_AUT + '</td>';
+            row += '<td>' + el.DNAIX_AUT + '</td>';
+            row += '<td>' + el.FK_NACIONALITAT + '</td>';
+            row += '<td class=\'taulaEditar\'>';
+            row += '<button type="button" class="btn btn-danger btn-block" value=' + el.ID_AUT + '><span class="glyphicon glyphicon-trash"></span> Borrar</button>';
+            row += '</td>';
+            row += '</tr>'
+            _table.append(row);
+        });
+        $.each(data.EXEMPLARS, function(index, el) {
+            var _table = $('#exemplarsModals');
+            var row;
+            row += '<tr>';
+            row += '<td>' + el.NUM_EXM + '</td>';
+            row += '<td>' + el.NREG + '</td>';
+            row += '<td>' + el.DATALTA_EXM + '</td>';
+            row += '<td>' + el.FK_UBICEXM + '</td>';
+            row += '<td class=\'taulaEditar\'>';
+            row += '<button type="button" class="btn btn-danger btn-block" value=' + el.NUM_EXM + '><span class="glyphicon glyphicon-trash"></span> Borrar</button>';
+            row += '</td>';
+            row += '</tr>'
+            _table.append(row);
+        });
+        afterLoadBook();
+    }).fail(function() {
+        console.log("error");
+    }).always(function() {
+        //console.log("complete");
+    });
+}
+
+function afterLoadBook() {
+    blockStuff(true);
+    $('#botoModal123').off("click").text("Editar");
+    $('#botoModal123').on("click", function() {
+        blockStuff(false);
+        $('.taulaEditar').show();
+        $(this).text('Guardar');
+        $(this).off("click").on("click", function() {
+            saveDataForm();
+            $('.alert').hide();
+        });
+    });
+    $('#autorsModal button').click(function() {
+        var FK_IDAUT = $(this).val();
+        var FK_IDLLIB = $('#CodiLlibre').val();
+        //console.log("FK_IDAUT: " + FK_IDAUT + "\nFK_IDLLIB: " + FK_IDLLIB);
+        deleteAutor(FK_IDLLIB, FK_IDAUT);
+    });
+    $('#exemplarsModals button').click(function() {
+        var NUM_EXM = $(this).val();
+        var FK_IDLLIB = $('#CodiLlibre').val();
+        deleteExemplar(FK_IDLLIB, NUM_EXM);
+    });
 }
 
 function cleanFormData() {
     $('#formLlibre').trigger("reset");
     $('option').remove();
     $('#myModal td').remove();
+    $('.alert').hide();
 }
 
 function blockStuff(value) {
     $("#myModal input, textarea, select").prop("disabled", value);
     $(".btn-danger").prop("disabled", value);
     $('.taulaEditar').hide();
-    $('.alert').hide();
 }
 
 function saveDataForm() {
@@ -185,27 +183,54 @@ function saveDataForm() {
 
 function updateBook(info) {
     $.ajax({
-            url: 'setDataBook.php',
-            type: 'GET',
-            dataType: 'html',
-            data: info,
-        })
-        .done(function(data) {
-            console.log("success");
-            console.log(data);
-            //var x =  "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>";
-            $('#formAlert').html(data).show();
-            $("#myModal").animate({
-                scrollTop: 0
-            }, "slow");
+        url: 'setDataBook.php',
+        type: 'GET',
+        dataType: 'html',
+        data: info,
+    }).done(function(data) {
+        $('#formAlert').html(data).show();
+        $("#myModal").animate({
+            scrollTop: 0
+        }, "slow");
+    }).fail(function() {}).always(function() {});
+}
 
+function deleteAutor(FK_IDLLIB, FK_IDAUT) {
+    $.ajax({
+        url: 'delDataBook.php',
+        type: 'GET',
+        dataType: 'html',
+        data: {
+            id_llib: FK_IDLLIB,
+            id_autor: FK_IDAUT
+        },
+    }).done(function(data) {
+        $('#autorsModal button[type="button"][value="' + FK_IDLLIB + '"]').parent().parent().remove();
+        $('#autorsAlert').html(data).show();
+        $("#myModal").animate({
+            scrollTop: 0
+        }, "slow");
+    }).fail(function() {
+        console.log("error");
+    }).always(function() {});
+}
 
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-        });
-
+function deleteExemplar(FK_IDLLIB, NUM_EXM) {
+    $.ajax({
+        url: 'delDataBook.php',
+        type: 'GET',
+        dataType: 'html',
+        data: {
+            id_llib: FK_IDLLIB,
+            num_exm: NUM_EXM
+        },
+    }).done(function(data) {
+        $('#exemplarsModals button[type="button"][value="' + NUM_EXM + '"]').parent().parent().remove();
+        $('#exemplarsAlert').html(data).show();
+        $("#myModal").animate({
+            scrollTop: 0
+        }, "slow");
+    }).fail(function() {
+        console.log("error");
+    }).always(function() {});
 }
